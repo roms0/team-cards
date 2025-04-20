cbff3a27ece13c1db62f2730d7b22d560af0fd77
klimov.roman10 <klimov.roman10@rwb.ru>
2025-04-20 11:37:59 +0300

    bun server

--- server.ts
+++ server.ts
@@ -1,23 +1,18 @@
+import homepage from "./index.html";
+
 const server = Bun.serve({
-  port: 4000,
-  fetch(req, server) {
-    console.log(req.url);
-    // use a library to parse cookies
-    server.upgrade(req, {
-      // this object must conform to WebSocketData
-      data: {},
-    });
-    return undefined;
+  routes: {
+    // ** HTML imports **
+    // Bundle & route index.html to "/". This uses HTMLRewriter to scan the HTML for `<script>` and `<link>` tags, run's Bun's JavaScript & CSS bundler on them, transpiles any TypeScript, JSX, and TSX, downlevels CSS with Bun's CSS parser and serves the result.
+    "/": homepage,
+    // Bundle & route dashboard.html to "/dashboard"
   },
-  websocket: {
-    async open(ws) {
-      setInterval(() => {
-        ws.send("hi");
-      }, 1000);
-    },
-    // handler called when a message is received
-    async message(ws, message) {},
-  },
+
+  // Prior to v1.2.3, the `fetch` option was used to handle all API requests. It is now optional.
+  // async fetch(req) {
+  //   // Return 404 for unmatched routes
+  //   return new Response("Not Found", { status: 404 });
+  // },
 });
 
-console.log(server.port);
+console.log(`Listening on ${server.hostname} ${server.url}`);
