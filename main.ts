5f32d05c4b9e3d2863b52e1d55c8ad8aabb4e648
klimov.roman10 <klimov.roman10@rwb.ru>
2025-04-26 16:38:25 +0300

    gradients cards and transforms

--- main.ts
+++ main.ts
@@ -17,9 +17,17 @@ const target = document.getElementById("slot");
 
 const title = new BehaviorSubject("");
 
+function safe_append(id: string, fragment: DocumentFragment) {
+  const target = document.getElementById(id);
+  if (target) {
+    target.append(fragment);
+    return target;
+  }
+}
+
 title.subscribe((text) => {
   if (target) {
-    target.textContent = text;
+    target.innerHTML = `<p>${text}</p>`;
   }
 });
 
@@ -31,50 +39,35 @@ fromEvent(document, "click")
   )
   .subscribe(title);
 
-function promises() {
-  try {
-    Promise.allSettled([
-      Promise.resolve(1),
-      Promise.reject(new Error("some error")),
-    ])
-      .then(console.log)
-      .catch(console.error);
-    Promise.race([Promise.resolve(1), Promise.reject(new Error("some error"))])
-      .then(console.log)
-      .catch(console.error);
-    Promise.all([Promise.resolve(1), Promise.reject(new Error("some error"))])
-      .then(console.log)
-      .catch(console.error);
-    Promise.race([Promise.resolve(1), Promise.reject(new Error("some error"))])
-      .then(console.log)
-      .catch(console.error);
-    Promise.any([Promise.resolve(1), Promise.reject(new Error("some error"))])
-      .then(console.log)
-      .catch(console.error);
-  } catch (e) {
-    console.error(e);
+const data = [
+  { title: "first", content: "description of a cool card", cost: 3 },
+  { title: "another", content: "some robust mechanics", cost: 12 },
+  { title: "third nice", content: "some robust mechanics", cost: 40 },
+  { title: "last ultimative", content: "some robust mechanics", cost: 63 },
+];
+
+const template = document.querySelector(
+  "#first-edition-card-template"
+) as HTMLTemplateElement;
+
+window.addEventListener("DOMContentLoaded", () => {
+  const fragment = document.createDocumentFragment();
+  for (const k of data) {
+    const item = template.content.cloneNode(true) as HTMLTemplateElement;
+    const container: HTMLElement | null = item.querySelector(
+      ".first-edition-card"
+    );
+    if (container) {
+      const factor = Math.random();
+      container.innerHTML = `<p><span class=${
+        factor > 0.5 ? "attention" : "resource"
+      }>${k.cost}</span> ${k.title}</p>`;
+      container.style.cssText = `animation-delay: ${
+        factor * 1000
+      }ms; animation-name: ${factor > 0.5 ? "attent" : "attent-reverse"}`;
+    }
+    fragment.append(item);
   }
-}
 
-// @ts-nocheck
-
-async function loop() {
-  // for (let i = 1; i < 13; i++) {
-  //   const k = await new Promise((res) => {
-  //     setTimeout(() => res(i), Math.random() * 1000);
-  //   });
-  //   console.log(k);
-  // }
-  const promises = [1, 2, 3, 4, 5].map(
-    (i) => () =>
-      new Promise((res) => {
-        setTimeout(() => res(i), Math.random() * 1000);
-      })
-  );
-  promises.slice(1).reduce((chain, promise) => {
-    return chain.then((value) => {
-      console.log(value);
-      return promise();
-    });
-  }, promises[0]());
-}
+  safe_append("a-deck-showcase", fragment);
+});
